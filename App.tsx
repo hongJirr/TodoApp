9247b1cba665a0256770f6d9ad4ec6269a73fdda
HongJirr <gozlsrmawl@naver.com>
2023-05-05 21:51:42 +0900

    Platform.select 이용해 플랫폼별 분기 처리 추가

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import DateHead from './components/DateHead';
 import AppTodo from './components/AddTodo';
@@ -9,9 +9,14 @@ function App(): JSX.Element {
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
-        <DateHead />
-        <Empty />
-        <AppTodo />
+        {/** Keyboard가 화면을 가리는것을 막기 위해서 사용. */}
+        <KeyboardAvoidingView
+          behavior={Platform.select({ios: 'padding'})}
+          style={styles.avoid}>
+          <DateHead />
+          <Empty />
+          <AppTodo />
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -19,6 +24,9 @@ function App(): JSX.Element {
 
 const styles = StyleSheet.create({
   block: {
+    flex: 1,
+  },
+  avoid: {
     flex: 1,
   },
 });
--- components/AddTodo.tsx
+++ components/AddTodo.tsx
@@ -1,13 +1,84 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Image,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  Platform,
+  Keyboard,
+} from 'react-native';
 
 export default function AddTodo() {
-  return <View style={style.block} />;
+  const [text, setText] = useState('');
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss();
+  };
+  const Button = (
+    <View style={styles.buttonStyle}>
+      <Image
+        source={require('../assets/images/add.png')}
+        style={styles.image}
+      />
+    </View>
+  );
+  return (
+    <View style={styles.block}>
+      <TextInput
+        style={styles.input}
+        placeholder="할일을 입력하세요"
+        onChangeText={setText}
+        value={text}
+        onSubmitEditing={onPress} // enter 눌렀을때도 함수 호출
+        returnKeyType="done"
+      />
+      {Platform.select({
+        ios: (
+          <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
+            {Button}
+          </TouchableOpacity>
+        ),
+        android: (
+          <View style={styles.circleWrapper}>
+            <TouchableNativeFeedback onPress={onPress}>
+              {Button}
+            </TouchableNativeFeedback>
+          </View>
+        ),
+      })}
+    </View>
+  );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   block: {
     height: 64,
-    backgroundColor: 'red',
+    padding: 11,
+    border: '#EF9A9A',
+    borderWidth: 1,
+    borderRadius: 10,
+    justifyContent: 'center',
+    margin: 20,
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: '#000',
+    paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 48,
+    height: 48,
+  },
+  circleWrapper: {
+    overflow: 'hidden',
+    borderRadius: 100,
   },
 });
